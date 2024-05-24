8e3065110f8b4c1ab3f9a68664f1c9187feabe2f
Sleepy <sanketapatil178@gmail.com>
2024-05-24 16:32:01 +0530

    Basics done

---
+++ p3_Layers.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+inputs=[[1,2,3,2.5],
+        [2,5,-1,2],
+        [-1.5,2.7,3.3,-0.8]]
+
+weights=[[0.2,0.8,-0.5,1.0],
+         [0.5,-0.91,0.26,-0.5],
+         [-0.26,-0.27,0.17,0.87]]
+biases=[2,3,0.5]
+
+weights2=[[0.1,-0.14,0.5],
+         [-0.5,0.12,-0.33],
+         [-0.44,0.73,-0.13]]
+biases2=[-1,2,-0.5]
+
+layer1_output=np.dot(inputs,np.array(weights).T)+biases
+print(layer1_output)
+#if input is given as it is the dot product will not happen as sizes are not matching so transpose the weights matrix.
+
+#layer 1 outputs will become layer 2 inputs
+
+layer2_outputs=np.dot(layer1_output,np.array(weights2).T)+biases2
+print(layer2_outputs)
